c1a4df75112c87dbb664db989f63016ac689f398
Kali <131460453+K44Ll@users.noreply.github.com>
2025-03-03 19:00:12 -0300

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        </header>
+        <h1 class='titulo1'>Kali's Store</h1>
+        <h2 class='titulo2'>Seja bem vindo!</h2>
+        <div class='div1'>
+          <p class='p1'>Edição de imagens, criação de sites e criação de aplicativos</p>
+          <p class='p1'>Alguns de nossos projetos:</p>
+          <p class='p1'>aplicativos <a src='https://github.com/K44Ll?tab=repositories'>aqui</a></p>
+          <p class='p1'>sites, use esse como exemplo</p>
+        </div>
+    </div>
+  );
+}
+
+export default App;
